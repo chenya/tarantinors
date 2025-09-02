427a557958a26c1e838ec28d0a95080b97851d80
Chen <Chen.Yachin@gmail.com>
2025-09-02 13:41:34 +0300

    add movie_award table migrations

---
+++ migrations/20250902103759_create_movie_award.up.sql
@@ -0,0 +1,10 @@
+-- Add up migration script here
+CREATE TABLE IF NOT EXISTS movie_award (
+    movie_id INT REFERENCES movie(id) ON DELETE CASCADE,
+    award_category_id INT REFERENCES award_category(id) ON DELETE CASCADE,
+    year INT NOT NULL,
+    recipient VARCHAR(128),
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (movie_id, award_category_id, year, recipient)
+);
