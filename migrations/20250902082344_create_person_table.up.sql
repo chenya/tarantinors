9523eacf49ae800b48d7728c929efaeb40c38148
Chen <Chen.Yachin@gmail.com>
2025-09-02 11:29:52 +0300

    add genre and person tables migrations and change the timestamp in role table

---
+++ migrations/20250902082344_create_person_table.up.sql
@@ -0,0 +1,7 @@
+-- Add up migration script here
+CREATE TABLE IF NOT EXISTS person (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(128) NOT NULL UNIQUE,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
