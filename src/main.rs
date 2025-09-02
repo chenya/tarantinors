7bff6db28b61ccdc6e11e81c3ff0a66eb0ef90ba
Chen <Chen.Yachin@gmail.com>
2025-09-02 10:51:49 +0300

    add some more initial files setup

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use axum::{response::Html, routing::get, Router};
+use axum::{response::Html, routing::get, Json, Router};
+use serde::{Deserialize, Serialize};
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
-    let app = Router::new().route("/", get(root));
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/hello", get(hello_msg));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
 
@@ -12,8 +15,22 @@ async fn main() {
 
 }
 
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Msg {
+    message: String,
+}
+
 #[tracing::instrument]
 async fn root() -> Html<&'static str> {
     tracing::info!("Sending hello world response");
     Html("<h1>Hello, World!</h1>")
+}
+
+#[tracing::instrument]
+async fn hello_msg() -> Json<Msg> {
+    tracing::info!("Sending json hello world response");
+
+    Json(Msg{
+        message: "Hello, World!".to_string(),
+    })
 }
