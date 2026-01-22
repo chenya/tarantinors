1300f74bc36f96ad69e7803464b431cfa93be862
Chen Yachin <chen.yachin@gmail.com>
2026-01-22 11:48:32 +0200

    change max_connections to 20

--- src/store.rs
+++ src/store.rs
@@ -1,13 +1,11 @@
 use sqlx::postgres::{PgPool, PgPoolOptions};
 
-use crate::movies::data::store::MoviesStore;
-
-pub async fn init_dbpool() -> Result<sqlx::Pool<sqlx::Postgres>, sqlx::Error> {
+async fn init_dbpool() -> Result<sqlx::Pool<sqlx::Postgres>, sqlx::Error> {
     let db_connection_str = std::env::var("DATABASE_URL")
         .unwrap_or_else(|_| "postgresql://postgres:1234@localhost:7777/tarantinodb".to_string());
 
     let db_pool = PgPoolOptions::new()
-        .max_connections(10)
+        .max_connections(20)
         .connect(&db_connection_str)
         .await;
 
@@ -17,7 +15,6 @@ pub async fn init_dbpool() -> Result<sqlx::Pool<sqlx::Postgres>, sqlx::Error> {
 #[derive(Clone, Debug)]
 pub struct Store {
     pub connection: PgPool,
-    pub movies_store: MoviesStore,
 }
 
 impl Store {
@@ -28,10 +25,9 @@ impl Store {
                 panic!("Failed to create database pool: {}", e);
             }
         };
-        let movies_store = MoviesStore::new(connection.clone());
+
         Self {
             connection,
-            movies_store,
         }
     }
 }
